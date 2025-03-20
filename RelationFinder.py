4a9031711c12664250cfd52339728620c1cf8838
Krzysztof <99942553+KrzysztofA@users.noreply.github.com>
2025-03-20 12:48:24 +0000

    Updates to RelationFinder
    - Now the key is also customizable, meaning user is not tied to the specific chat server
    - Dataset Parser got created, it will go through nan values and fit them into the dataset. Right now the majority of features are implemented and being tested

--- RelationFinder.py
+++ RelationFinder.py
@@ -8,11 +8,11 @@ class RelationFinder:
     """
     Class used to perform searches on different relations between the main keyword and the relation
     """
-    def __init__(self, keyword: str, base_url: str = "http://localhost:11434/v1", model: str="phi4", temperature: int = 0, reasoning_model=False):
+    def __init__(self, keyword: str, base_url: str = "http://localhost:11434/v1", model: str="phi4", temperature: int = 0, api_key:str="ollama", reasoning_model=False):
         self.scraper = SearchEngineScraper()
         self.llm = Client(
                     base_url=base_url,
-                    api_key="ollama"
+                    api_key=api_key
         )
         self.key = keyword
         self.model = model
@@ -22,19 +22,23 @@ class RelationFinder:
     def find_relation_json_text(self, relation: str):
         """Find the desired relation about the given, main key"""
         relation_data = self.scraper.get_query_results_text(f"{self.key} {relation}")
+
+        main_msg = f"""Extract the following data point: \"{relation}\", about the: \"{self.key}\"\n
+                       From this pile of text:\n
+                       {relation_data},
+                       in the json format, as \"{self.key}\": {{
+                           "{relation}": "answer"
+                       }}.
+
+                       Be as concise as possible, avoid any irrelevant data. Deliver only the asked data point ({relation}) or null in a json format.
+                       Never include any explanation or anything besides the json.
+                    """
+
+
         msgs = [
         {
             "role": "user",
-            "content": f"""Extract the following data point: \"{relation}\", about the: \"{self.key}\"\n
-            From this pile of text:\n
-            {relation_data},
-            in the json format, as \"{self.key}\": {{
-                "{relation}": "answer"
-            }}.
-
-            Be as concise as possible, avoid any irrelevant data. Deliver only the asked data point ({relation}) or null in a json format.
-            Never include any explanation or anything besides the json.
-            """
+            "content": main_msg
         }]
         completion = self.llm.chat.completions.create(model=self.model, messages=msgs, temperature=self.temperature)
         return completion.choices[0].message.content if not self.reasoning_model else completion.choices[0].message.content.split("</think>")[1]
@@ -45,10 +49,17 @@ class RelationFinder:
         """
         json_text = self.find_relation_json_text(relation)
         json_match = re.search(r'```json\s*(.*?)\s*```', json_text, re.DOTALL)
-        dictionary = json.loads(json_match.group(1))
+        try:
+            dictionary = json.loads(json_match.group(1))
+        except:
+            try:
+                dictionary = json.loads(json_text)
+            except:
+                print(f"CRITICAL ERROR ({json_text})")
+                dictionary = {}
         return dictionary
 
 if __name__ == "__main__":
-    test = RelationFinder("Cynexis Insight")
-    print(test.find_relation_dictionary("Founders"))
-    
+    test = RelationFinder("Anthropic PBC", model="phi4")
+    print(test.find_relation_dictionary("Competitor Analysis"))
+    print(test.find_relation_dictionary("Media Mentions"))
