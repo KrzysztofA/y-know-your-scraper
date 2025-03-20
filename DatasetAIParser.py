7bf7730bab6282b07144dab03517059624078d06
Krzysztof <99942553+KrzysztofA@users.noreply.github.com>
2025-03-20 15:24:53 +0000

    Renamed DatasetParser to DatasetAIParser
    - Renamed DatasetParser to DatasetAIParser to make it clear it uses AI to parse the data
    - Fixed commonly occurring errors in SearchEngineScraper

---
+++ DatasetAIParser.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from RelationFinder import RelationFinder
+
+class DatasetAIParser:
+    """
+    Class responsible for parsing the dataset
+    """
+    def __init__(self, file_path: str):
+        self.data = pd.read_csv(file_path)
+        self.file_path = file_path
+
+    def fill_missing_data_with_ai_at_index(self, index:int, model="phi4"):
+        """
+        Fills the missing data using provided llm and appends this data to the given CSV TODO
+        """
+        
+        # Extract company
+        company = self.data.iloc[index, 0]
+        
+        # Create relationship finder instance
+        company_relation_finder = RelationFinder(company, model=model)
+        
+        # Enumerate values from the given row
+        for i, val in enumerate(self.data.iloc[index, 1:30]):
+            
+            # Find null values and attempt to fill them with LLM
+            if pd.isnull(val):
+                try:
+                    column = self.data.columns[i + 1]
+                    print(f"Starting scraping null value {column} at index {i} for a company {company}")
+                    data_scraped = company_relation_finder.find_relation_dictionary(column)
+                    null_data = data_scraped[company][column]
+                    self.data.iloc[index, i+1] = self.data.iloc[index, i+1].astype(str)
+                    self.data.iloc[index, i+1] = str(null_data)
+                    print(f"Data obtained: {null_data}")
+                except:
+                    print("Error occured, continuing to the next field")
+                    continue
+
+    def finish_operations(self):
+        new_file_path = self.file_path.replace(".csv", " - AI Modified.csv")
+        self.data.to_csv(new_file_path)
+
+
+if __name__ == "__main__":
+    data_parser = DatasetAIParser("knowYourAi - Company Details.csv")
+    data_parser.fill_missing_data_with_ai_at_index(0)
+    data_parser.finish_operations()
--- SearchEngineScraper.py
+++ SearchEngineScraper.py
@@ -8,23 +8,27 @@ import requests
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
+
 class SearchEngineScraper:
     """
     Class searching for a query on a given Seach Engine and returning the results' links and text of the first page
     """
-    def __init__(self, search_engine:str='https://html.duckduckgo.com/html'):
+
+    def __init__(self, search_engine: str = "https://html.duckduckgo.com/html"):
         options = Options()
-        options.add_argument('--headless=new') 
+        options.add_argument("--headless=new")
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(search_engine)
-        if search_engine == 'https://www.google.com/':
+        if search_engine == "https://www.google.com/":
             self.__cookies_check_reject()
 
     def __cookies_check_reject(self):
         """For google.com, checks for cookies and reject them, irrelevant for duckduckgo.com, and as google is blocked by captcha, not used"""
         try:
-            WebDriverWait(self.browser, 100).until(EC.element_to_be_clickable((By.ID, "W0wltc")))
-            cookies_button_reject = self.browser.find_element(By.ID, 'W0wltc')
+            WebDriverWait(self.browser, 100).until(
+                EC.element_to_be_clickable((By.ID, "W0wltc"))
+            )
+            cookies_button_reject = self.browser.find_element(By.ID, "W0wltc")
             cookies_button_reject.click()
         except Exception as e:
             print(f"Error: {e}")
@@ -36,35 +40,38 @@ class SearchEngineScraper:
         """
 
         # Wait for the search field to be present
-        WebDriverWait(self.browser, 100).until(EC.presence_of_element_located((By.NAME, "q")))
-        search_text_field = self.browser.find_element(By.NAME, 'q')
+        WebDriverWait(self.browser, 100).until(
+            EC.presence_of_element_located((By.NAME, "q"))
+        )
+        search_text_field = self.browser.find_element(By.NAME, "q")
         search_text_field.clear()
         search_text_field.send_keys(query)
         search_text_field.submit()
 
         # Wait for the results to be present
-        WebDriverWait(self.browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
-        results = self.browser.find_elements(By.CLASS_NAME, 'result')
+        WebDriverWait(self.browser, 100).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "result"))
+        )
+        results = self.browser.find_elements(By.CLASS_NAME, "result")
         urls = []
         descriptions = []
 
         # Iterate over results and extract the url and the description
         for result in results:
-            url = result.find_element(By.TAG_NAME, 'a')
-            description = result.find_element(By.CLASS_NAME, 'result__snippet')
+            url = result.find_element(By.TAG_NAME, "a")
+            description = result.find_element(By.CLASS_NAME, "result__snippet")
             descriptions.append(description.text)
-            urls.append(url.get_attribute('href'))
+            urls.append(url.get_attribute("href"))
         return dict(zip(urls, descriptions))
 
     def search_multiple(self, query):
         """
         Searches for multiple queries and return the results
         """
-        
+
         # Check if query is a string or a list, if is a string, convert to list
         if isinstance(query, str):
             query = [query]
-        
         # Search for the query and save the results as a dictionary
         ret = {}
         for q in query:
@@ -83,21 +90,20 @@ class SearchEngineScraper:
 
         # Create file if does not exist
         if not os.path.exists(file_name):
-            with open(self.file_name, 'w+') as file:
+            with open(self.file_name, "w+") as file:
                 json.dump({}, file)
-        
+
         # Load the file
         data = {}
-        with open(file_name, 'r') as file:
+        with open(file_name, "r") as file:
             data = dict(json.load(file))
 
         # Update the file with the new data
         data.update(to_save)
 
         # Override the file
-        with open(file_name, 'w') as file:
+        with open(file_name, "w") as file:
             json.dump(data, file)
-
 
     def get_query_results_text(self, query: str):
         """
@@ -112,9 +118,12 @@ class SearchEngineScraper:
         for url in results.keys():
             try:
                 # Use beautiful soup to get the text
-                html = requests.get(url).text
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36"
+                }
+                html = requests.get(url, headers=headers).text
                 soup = BeautifulSoup(html, "lxml")
-                soup = soup.find_all('article')
+                soup = soup.find_all("article")
                 relevant_text.extend(soup)
             except:
                 # Try using selenium to get the text
@@ -125,18 +134,21 @@ class SearchEngineScraper:
                 for el in elems:
                     relevant_text.append(el.text)
 
-
         # Extract the text from all the paragraphs
         texts = []
         for text in relevant_text:
-            soup = BeautifulSoup(text.text, "lxml")
+            if isinstance(text, str):
+                soup = BeautifulSoup(text, "lxml")
+            else:
+                soup = BeautifulSoup(text.text, "lxml")
+
             texts.append(soup.get_text())
         return texts
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     scraper = SearchEngineScraper()
-#    print(scraper.search('javascript'))
-    print(scraper.get_query_results_text('FuturAI Industry'))
+    #    print(scraper.search('javascript'))
+    print(scraper.get_query_results_text("Python"))
 #    print(scraper.search_multiple(['python', 'c++']))
 #    scraper.search_multiple_to_JSON(['python', 'c#'], 'test.json')
